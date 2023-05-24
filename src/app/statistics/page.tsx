8426730543f938391436d9bb6dca57c9b4246a2a
Vinicius <vinicezar800@hotmail.com>
2023-05-24 15:26:54 -0300

    feat: change layout if statistics is not selected

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import { useCallback, useContext, useEffect, useState } from 'react'
 import LineUp from '@/components/LineUp'
 import { TeamContext } from '@/context/TeamContext'
@@ -7,7 +8,6 @@ import { Statistics } from '@/types/apiFootballTypes'
 import { api } from '@/lib/api'
 import ResultTable from '@/components/ResultTable'
 import { GoalMinutesChart } from '@/components/GoalMinutesChart'
-import Link from 'next/link'
 
 export default function StatisticsPage() {
   const { selectedOptions } = useContext(TeamContext)
@@ -47,22 +47,40 @@ export default function StatisticsPage() {
 
   return (
     <>
-      <div className="flex items-center">
-        <Link
-          href="/statistics/countries"
-          className="inline-block rounded-lg bg-green-600 p-2 font-alt text-white outline-none transition hover:bg-green-700 focus:bg-green-700"
-        >
-          Selecionar outro país
-        </Link>
-      </div>
+      {!statistics ? (
+        <>
+          <h1 className="text-center font-alt text-xl">
+            Você ainda não selecionou o seu time!
+          </h1>
+          <div className="flex h-full items-center justify-center">
+            <Link
+              href="/statistics/countries"
+              className="inline-block rounded-lg bg-green-600 p-2 font-alt text-white outline-none transition hover:bg-green-700 focus:bg-green-700"
+            >
+              Selecionar time
+            </Link>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex items-center">
+            <Link
+              href="/statistics/countries"
+              className="inline-block rounded-lg bg-green-600 p-2 font-alt text-white outline-none transition hover:bg-green-700 focus:bg-green-700"
+            >
+              Selecionar outro país
+            </Link>
+          </div>
 
-      <div className="grid grid-cols-2 gap-4">
-        <div className="flex flex-col gap-4">
-          <ResultTable fixtures={fixtures} />
-          <GoalMinutesChart goalMinutes={goalMinutes} />
-        </div>
-        <LineUp lineups={lineUps} />
-      </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col gap-4">
+              <ResultTable fixtures={fixtures} />
+              <GoalMinutesChart goalMinutes={goalMinutes} />
+            </div>
+            <LineUp lineups={lineUps} />
+          </div>
+        </>
+      )}
     </>
   )
 }
